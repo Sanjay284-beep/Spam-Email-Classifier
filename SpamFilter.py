e64ddd7e819252525524df6a781916b94725090d
Narra sanjay <sanjaynarra23@gmail.com>
2026-08-18 11:38:43 +0530

    Remove unnecessary manual data shuffle

--- SpamFilter.py
+++ SpamFilter.py
@@ -58,10 +58,6 @@ def preprocess():
     cols = data.shape[1]-1
     X = data[:,0:cols]
     Y = data[:,cols]
-    indices = np.arange(X.shape[0])
-    np.random.shuffle(indices)
-    X = X[indices]
-    Y = Y[indices]
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
     text.insert(END,"Total spam & non spam records found in dataset is : "+str(len(X))+"\n")
     text.insert(END,"Splitting dataset into train & test. ML will user 80% dataset size for training and 20% for testing\n")
